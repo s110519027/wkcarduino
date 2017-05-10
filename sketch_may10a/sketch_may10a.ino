96cf29aa2e33692c4434d9433b5599c0634a570b
s110519027 <ben0939637@gmail.com>
2017-05-10 12:11:31 +0800

    Add files via upload

---
+++ sketch_may10a/sketch_may10a.ino
@@ -0,0 +1,59 @@
+
+byte seven_seg_digits[16][7]={{1,1,1,1,1,1,0},//=0
+{0,1,1,0,0,0,0},//=1
+{1,1,0,1,1,0,1},//=2
+{1,1,1,1,0,0,1},//=3
+{0,1,1,0,0,1,1},//=4
+{1,0,1,1,0,1,1},//=5
+{1,0,1,1,1,1,1},//=6
+{1,1,1,0,0,0,0},//=7
+{1,1,1,1,1,1,1},//=8
+{1,1,1,0,0,1,1},//=9
+{1,1,1,0,1,1,1},//=10(A)
+{0,0,1,1,1,1,1},//=11(b)
+{0,0,0,1,1,0,1},//=12(c)
+{0,1,1,1,1,0,1},//=13(d)
+{1,0,0,1,1,1,1},//=14(E)
+{1,0,0,0,1,1,1}//=15(F)
+};
+const int buttonPin=12;
+int buttonState=0;
+int num=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+digitalWrite(9,0);
+digitalWrite(buttonPin,LOW);
+pinMode(buttonPin, INPUT);
+}
+
+void sevenSegWrite(byte digit) {
+  // put your main code here, to run repeatedly:
+  byte pin=2;
+  for(byte seg=0;seg<7;++seg){
+    digitalWrite(pin,
+    seven_seg_digits[digit][seg]);
+    ++pin;
+    }
+  }
+  void loop(){
+ buttonState=digitalRead(buttonPin);
+
+ if(buttonState==HIGH){
+  num++;
+  if(num>15){
+     num=0;
+    }
+  }
+   sevenSegWrite(num);
+  delay(1000);
+
+ }
